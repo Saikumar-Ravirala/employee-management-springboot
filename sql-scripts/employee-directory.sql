98ca52cb335887e77bb4a1219426f90369d8ec4f
Saikumar-Ravirala <saikumarravirala1@gmail.com>
2025-07-17 10:49:28 +0530

    Update employee-directory.sql

--- sql-scripts/employee-directory.sql
+++ sql-scripts/employee-directory.sql
@@ -20,9 +20,9 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` VALUES 
-	(1,'Leslie','Andrews','leslie@luv2code.com'),
-	(2,'Emma','Baumgarten','emma@luv2code.com'),
-	(3,'Avani','Gupta','avani@luv2code.com'),
-	(4,'Yuri','Petrov','yuri@luv2code.com'),
-	(5,'Juan','Vega','juan@luv2code.com');
+	(1,'saikumar','ravirala','sai@gmail.com'),
+	(2,'chris','gayle','chris@circket.com'),
+	(3,'lewis','hamilton','lewis@f1.com');
+	
+	
 
